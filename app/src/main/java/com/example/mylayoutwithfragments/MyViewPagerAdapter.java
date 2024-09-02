1a7af49195b8a4fb7118b5e9cc85ee33e054d1df
samiabo75 <31937339+samiabo75@users.noreply.github.com>
2024-09-02 11:38:58 +0300

    my layout with fragments

---
+++ app/src/main/java/com/example/mylayoutwithfragments/MyViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.mylayoutwithfragments;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.mylayoutwithfragments.Fragment.HomeFragment;
+import com.example.mylayoutwithfragments.Fragment.NotificationFragment;
+import com.example.mylayoutwithfragments.Fragment.SettingsFragment;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+    public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new NotificationFragment();
+            case 2:
+                return new SettingsFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
